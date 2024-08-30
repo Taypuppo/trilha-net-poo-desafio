d2b987d6d7259731c07a0cf5a4c5d885752a1324
Taypuppo <taypuppo@icloud.com>
2024-08-30 08:28:17 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "2584", modelo: "Nokia X-30", imei: "NO-1603", memoria: 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "1406", modelo: "Iphone 7", imei: "I-1706", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Whatsapp");
